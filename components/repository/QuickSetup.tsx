744d8125a2b94db549aff51475e663f218bc3aab
asolovov <andre.solovov@gmail.com>
2023-02-20 13:25:37 +0400

    GIT-51: Bug: can't select anything in quick setup instructions

--- components/repository/QuickSetup.tsx
+++ components/repository/QuickSetup.tsx
@@ -9,21 +9,21 @@ const QuickSetup = ({url}: {url: string }) => {
             <h4 className={"mt-3"}>Create new repository on the command line</h4>
             <div className={"card mt-3"}>
                 <div className={"card-body"}>
-                    <samp className={"card-text user-select-all"}>echo &quot;# qqq&quot; &gt;&gt; README.md</samp><br/>
-                    <samp className={"card-text user-select-all"}>git init</samp><br/>
-                    <samp className={"card-text user-select-all"}>git add README.md</samp><br/>
-                    <samp className={"card-text user-select-all"}>git commit -m &quot;first commit&quot;</samp><br/>
-                    <samp className={"card-text user-select-all"}>git branch -M main</samp><br/>
-                    <samp className={"card-text user-select-all"}>git remote add origin {url}</samp><br/>
-                    <samp className={"card-text user-select-all"}>git push -u origin main</samp>
+                    <samp className={"card-text"}>echo &quot;# qqq&quot; &gt;&gt; README.md</samp><br/>
+                    <samp className={"card-text"}>git init</samp><br/>
+                    <samp className={"card-text"}>git add README.md</samp><br/>
+                    <samp className={"card-text"}>git commit -m &quot;first commit&quot;</samp><br/>
+                    <samp className={"card-text"}>git branch -M main</samp><br/>
+                    <samp className={"card-text"}>git remote add origin {url}</samp><br/>
+                    <samp className={"card-text"}>git push -u origin main</samp>
                 </div>
             </div>
             <h4 className={"mt-3"}>Push an existing repository form the command line</h4>
             <div className={"card mt-3"}>
                 <div className={"card-body"}>
-                    <samp className={"card-text user-select-all"}>git remote add origin {url}</samp><br/>
-                    <samp className={"card-text user-select-all"}>git branch -M main</samp><br/>
-                    <samp className={"card-text user-select-all"}>git push -u origin main</samp>
+                    <samp className={"card-text"}>git remote add origin {url}</samp><br/>
+                    <samp className={"card-text"}>git branch -M main</samp><br/>
+                    <samp className={"card-text"}>git push -u origin main</samp>
                 </div>
             </div>
         </div>
