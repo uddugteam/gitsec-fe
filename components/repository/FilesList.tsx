13c36b023ac6721ce6d0ee99636868173595c8dd
asolovov <andre.solovov@gmail.com>
2023-02-20 23:17:59 +0400

    BugFix and fetch errors handling

--- components/repository/FilesList.tsx
+++ components/repository/FilesList.tsx
@@ -1,12 +1,12 @@
 import {IpfsType} from "@/types/ipfsType";
 import FileRow from "@/components/repository/FileRow";
 
-const FilesList = ({ipfs, setLoading}: {ipfs: IpfsType, setLoading: Function}) => {
+const FilesList = ({ipfs, setLoading, id}: {ipfs: IpfsType, setLoading: Function, id: string}) => {
     return (
         <table className="table mt-3">
             <tbody>
             {ipfs.content.map((value, index) =>
-                <FileRow hash={value.hash} name={value.name} commit={value.commit} timestamp={value.timestamp} setLoading={setLoading} key={index}/>
+                <FileRow id={id} hash={value.hash} name={value.name} commit={value.commit} timestamp={value.timestamp} setLoading={setLoading} key={index}/>
             )}
             </tbody>
         </table>
